211a0018f224fc80213d9b5dc6d3828ccf4ae7ba
Emmanuel <emmapraise@gmail.com>
2022-05-05 17:51:23 +0100

    Created Abstract Model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 
-class Stock(models.Model):
+class common(models.Model):
+    createat = models.DateTimeField(auto_now_add=True, null=True)
+    updateat = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Stock(common):
     name = models.CharField(max_length=255)
     symbol = models.CharField(max_length=100)
     exchange = models.CharField(max_length=100)
@@ -11,7 +19,7 @@ class Stock(models.Model):
         return self.name
 
 
-class Mention(models.Model):
+class Mention(common):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     message = models.TextField()
@@ -22,7 +30,7 @@ class Mention(models.Model):
         return f"The source is ({self.source}) and the url is ({self.url})"
 
 
-class Stock_price(models.Model):
+class Stock_price(common):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     date = models.DateTimeField()
     open = models.DecimalField(decimal_places=2, max_digits=10)
@@ -35,7 +43,7 @@ class Stock_price(models.Model):
         return f"Stock opened at ({self.open}) and close at ({self.close})"
 
 
-class Etf_holdings(models.Model):
+class Etf_holdings(common):
     etf_id = models.IntegerField()
     holding_id = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
